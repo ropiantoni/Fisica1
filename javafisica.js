ab5b783138cefcb4d5ad004fb9f69d81955839a4
ropiantoni <rociopiantoni@hotmail.com>
2016-06-02 10:08:33 -0300

    javascript empezando :(

---
+++ javafisica.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ropiantoni & juli bertorello on 2/6/16.
+ */
+function comprobarNum(valorIngreso, nombreParametro) {
+    if (isNaN(valorIngreso)) {
+        alert("Valor ingresado no numerico, ingresar de nuevo.");
+
+        var el = document.getElementById("formCalcular").elements;
+        el[nombreParametro].value = "";
+    }
+}
+function calcularDistancia(){
+    di=df
+}
