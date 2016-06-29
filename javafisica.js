77a282d9f7c895eb0fdccfa635616ad70c83e3b3
ropiantoni <rociopiantoni@hotmail.com>
2016-06-29 18:54:06 -0300

    barra de abajo y parsefloat

--- javafisica.js
+++ javafisica.js
@@ -4,20 +4,6 @@
  * @author Piantoni & Bertorello
  * @version 0.1
  */
-/**
- * @fileoverview Libreria Js Para problemas de encuentro
- * @author Piantoni & Bertorello
- * @version 0.1
- */
-
-/**
- * Comprueba que los valores ingresados sean Numéricos, sino, envia mensaje al usuario y blanquea el campo de texto
- * @method comprobarNum
- * @param valorIngreso, valor ingresado que puede ser altura, masa, distancia o Indice de Rozamiento
- * @param nombreParametro, que puede ser altura, masa, distanciaFreno o CoeficienteRozamiento
- */
-
-
 
     var velocidad1;
     var velocidad2;
@@ -25,7 +11,7 @@
     var posicion2;
     var distancia;
 
-
+//comprueba que el valor ingresado sea un numero, sino aparece una alerta por pantalla
 function comprobarNum(valorIngreso, nombreParametro) {
     if (isNaN(valorIngreso)) {
         alert("Valor ingresado no numerico, ingresar de nuevo.");
@@ -34,7 +20,9 @@ function comprobarNum(valorIngreso, nombreParametro) {
 
 }
 
-function convertirUnidades (valor, unidad){ //CONVIERTE LAS UNIDADES DE VALORES AL CONVENCIONAL
+
+//funcion que va a transforma los valores a km o a km/h
+function convertirUnidades (valor, unidad){
     if(unidad == "m"){
         valor = valor/1000;
 
@@ -45,19 +33,23 @@ function convertirUnidades (valor, unidad){ //CONVIERTE LAS UNIDADES DE VALORES 
     }
     return valor;
 }
-/**
- * Calcula la distancia a la que se produce el encuentro o el tiempo que demora, según la opción que ingresa el usuario
- */
-function calculo (){
-    var unidadPos1 = document.getElementById("Uposicion1").value;
-    var unidadVel1 = document.getElementById("Uvelocidad1").value;
-    var unidadPos2 = document.getElementById("Uposicion2").value;
-    var unidadVel2 = document.getElementById("Uvelocidad2").value;
-    velocidad1 = document.getElementById("vel-uno").value;
-    velocidad2 = document.getElementById("vel-dos").value;
-    posicion1 = document.getElementById("pos-uno").value;
-    posicion2 = document.getElementById("pos-dos").value;
 
+//Calcula la distancia a la que se produce el encuentro o el tiempo que demora, según la opción que ingresa el usuario
+
+function calculo (){
+    
+    var unidadPos1 = parseFloat(document.getElementById("Uposicion1").value.replace(",","."));
+    var unidadVel1 = parseFloat(document.getElementById("Uvelocidad1").value.replace(",","."));
+    var unidadPos2 = parseFloat(document.getElementById("Uposicion2").value.replace(",","."));
+    var unidadVel2 = parseFloat(document.getElementById("Uvelocidad2").value.replace(",","."));
+    
+    velocidad1 = parseFloat(document.getElementById("vel-uno").value.replace(",","."));
+    velocidad2 = parseFloat(document.getElementById("vel-dos").value.replace(",","."));
+    posicion1 = parseFloat(document.getElementById("pos-uno").value.replace(",","."));
+    posicion2 = parseFloat(document.getElementById("pos-dos").value.replace(",","."));
+
+
+    //si la unidad es distinta a km, o km/h, llamo a la funcion convertirUnidades
     if(unidadPos1 != "km"){
         posicion1= convertirUnidades(posicion1, unidadPos1);
     }
@@ -78,6 +70,9 @@ function calculo (){
     tiempo = (posicion1 - posicion2) / (velocidad2 - velocidad1);
     tiempo = tiempo * 3600;
     distancia = posicion1 + velocidad1 * Number(tiempo);
+    
+    //dependiendo de la opcion elegida, muestra el dato correspondiente
+    
     if(op == "DISTANCIA") {
         document.getElementById("resultado").value = distancia + "m";
     }
@@ -98,7 +93,7 @@ function dibujar() {
     var a2 = myCanvas.getContext("2d");
     var centerY = myCanvas.height / 2;
     var auto1 = new Image(100,20);
-    var auto2 = new Image(200,25);
+    var auto2 = new Image(200,20);
     auto1.src = "Imagenes/auto.jpg";
     auto2.src = "Imagenes/auto2.jpg";
 
@@ -110,21 +105,21 @@ function dibujar() {
     function animar(){
 
         myCanvas.width = myCanvas.width;
-        // auto 1
+        // dibujo el auto 1
         a1.beginPath();
         a1.drawImage(auto1,x1,centerY-(auto1.height/2));
         a1.stroke();
-        //auto 2
+        //dibujo el auto 2
         a2.beginPath();
         a2.drawImage(auto2,x2,centerY-(auto2.height/2));
         a2.stroke();
 
 
-        // calle
+        // dibujo la calle
         calle.moveTo(0,centerY+30);
         calle.lineTo(700,centerY+30);
         calle.stroke();
-
+        
         x1 += velocidad1*0.05;
         x2 +=velocidad2*0.05;
 
